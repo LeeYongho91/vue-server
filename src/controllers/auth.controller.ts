84a8b7cfac2b2a5232658226fa7f2b5805dadc88
Essence <skyks4848@naver.com>
2021-07-25 19:52:31 +0900

    20210725

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -37,6 +37,7 @@ class AuthController {
    */
   public logIn = (req: Request, res: Response, next: NextFunction) => {
     try {
+      console.log(req.body);
       // 아까 local로 등록한 인증과정 실행
       passport.authenticate('local', (passportError, user, info) => {
         // 인증이 실패했거나 유저 데이터가 없다면 에러 발생
