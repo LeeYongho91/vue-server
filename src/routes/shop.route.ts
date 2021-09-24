4b03c351467d7f1db99c07ac16ab0b8cfa1f714d
Essence <skyks4848@naver.com>
2021-09-24 19:06:48 +0900

    20210924

--- src/routes/shop.route.ts
+++ src/routes/shop.route.ts
@@ -15,6 +15,11 @@ class ShopRoute implements Route {
   private initializeRoutes() {
     this.router.get(`${this.path}/:params`, this.ShopController.getProducts);
     this.router.post(`${this.path}/detail`, this.ShopController.getProductsById);
+    this.router.post(`${this.path}/LikeProducts`, this.ShopController.getLikeProducts);
+    this.router.post(`${this.path}/qa/post`, this.ShopController.postProductQa);
+    this.router.post(`${this.path}/qa`, this.ShopController.getProductQa);
+    this.router.post(`${this.path}/review`, this.ShopController.getProductReview);
+    this.router.post(`${this.path}/review/post`, this.ShopController.postProductReview);
   }
 }
 
