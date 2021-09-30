31b14343176452385cacf2e3b2b4ef874dc7df32
Essence <skyks4848@naver.com>
2021-09-30 19:42:50 +0900

    20210930

--- src/controllers/shop.controller.ts
+++ src/controllers/shop.controller.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { Product } from '@/interfaces/shop/product.interface';
+import { Qa } from '@/interfaces/shop/qa.interface';
+import { QaAnswer } from '@/interfaces/shop/qa_answer.interface';
+import { Review } from '@/interfaces/shop/review.interface';
 import shopService from '@services/shop.service';
 
 class ShopController {
@@ -23,14 +26,107 @@ class ShopController {
     }
   };
 
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
   public getProductsById = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id = req.body.id;
       const findOneProduct = await this.shopService.getProductsById(id);
+      const reviewInfo = await this.shopService.getProductReviewInfo(id);
 
-      console.log(findOneProduct);
+      res.status(200).json({ product: findOneProduct, reviewInfo, message: 'findOne' });
+    } catch (error) {
+      next(error);
+    }
+  };
 
-      res.status(200).json({ product: findOneProduct, message: 'findOne' });
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  public getLikeProducts = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const Products: Array<number> = await this.shopService.getLikeProducts();
+
+      res.status(200).json({ Products, message: 'getLikeProducts' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  public postProductQa = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+
+      const result: Qa = await this.shopService.postProductQa(data);
+      res.status(200).json({ result, message: 'postProductQa' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  public getProductQa = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const qaList: Qa[] = await this.shopService.getProductQa(data);
+      const qaAnswerList: QaAnswer[] = await this.shopService.getProductQaAnswer(data);
+
+      const qaListCount: Number = await this.shopService.getProductQaCount(data);
+
+      res.status(200).json({ qaList, qaAnswerList, count: qaListCount, message: 'getProductQa' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  public getProductReview = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const reviewList: Review[] = await this.shopService.getProductReview(data);
+      const reviewListCount: Number = await this.shopService.getProductReviewCount(data);
+
+      res.status(200).json({ reviewList, count: reviewListCount, message: 'getProductReview' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  public postProductReview = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const result: Review = await this.shopService.postProductReview(data);
+
+      res.status(200).json({ result, message: 'postProductReview' });
     } catch (error) {
       next(error);
     }
